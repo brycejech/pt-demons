3e481785a8b8c9e125913d3be5c8c5c820e32b5c
Bryce Jech <bryce@brycejech.com>
2019-04-08 20:25:05 -0500

    app pages

---
+++ client/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template lang="html">
+    <div class="container container-lg">
+        <sign-up-form />
+    </div>
+</template>
+
+<script>
+'use strict';
+
+import SignUpForm from '@/components/SignUpForm';
+
+export default {
+    name: 'home',
+    components: {
+        SignUpForm
+    }
+}
+
+</script>
+
+<style lang="sass" scoped>
+
+</style>
